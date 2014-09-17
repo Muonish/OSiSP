9287b467314f735c7512b59a0f886c0389f2ddf8
Muonish <breshko314@gmail.com>
2014-09-17 23:00:59 +0300

    lab1_row_part

--- Lab1Win32/Lab1Win32/PaintWin32.cpp
+++ Lab1Win32/Lab1Win32/PaintWin32.cpp
@@ -8,18 +8,27 @@
 
 // Global variables
 #define WM_MOUSEMOVE 0x0200
-#define W_PEN 1002
-#define W_BRUSH 1003
-#define W_LINE 1004
-#define W_CURVE 1005
-#define W_ELLIPSE 1006
-#define W_RECTANGLE 1007
-
+#define W_OPEN 1001
+#define W_SAVE 1002
+#define W_EXIT 1003
+#define W_PEN 1004
+#define W_BRUSH 1005
+#define W_LINE 1006
+#define W_CURVE 1007
+#define W_ELLIPSE 1008
+#define W_RECTANGLE 1009
+#define W_P3 1010
+#define W_P4 1011
+#define W_P5 1012
+#define W_P6 1013
+#define W_COLORPEN 1014
+#define W_COLORBRUSH 1015
+#define W_CLEAR 1016
+#define W_PRINT 1017
 
 static TCHAR szWindowClass[] = _T("win32app");			// The main window class name.
 static TCHAR szTitle[] = _T("Win32 Paint Application");
-static enum tools {PEN, BRUSH} currentTool = PEN;
-static enum figures {NONE, LINE, CURVE, ELLIPSE, RECTANGLE} currentFigure = NONE;
+static enum figures {NONE, PEN, LINE, CURVE, ELLIPSE, RECTANGLE,TEXT, P3, P4, P5, P6} currentFigure = NONE;
 BOOL fTracking = FALSE;
 POINTS ptsBegin;
 HDC hdc;
@@ -35,7 +44,7 @@ HPEN Pen;
 HINSTANCE hInst;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-int MouseMoveAction(HWND, LPARAM, POINTS *, figures , tools );
+int MouseMoveAction(HWND, LPARAM, POINTS *, figures);
 
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
@@ -84,16 +93,31 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	HMENU menu_file = CreatePopupMenu();
 	HMENU menu_draw = CreatePopupMenu();
 	HMENU menu_figure = CreatePopupMenu();
+	HMENU menu_polygon = CreatePopupMenu();
+	HMENU menu_color = CreatePopupMenu();
+	HMENU menu_canvas = CreatePopupMenu();
 	AppendMenu(main_menu, MF_STRING | MF_POPUP, (UINT)menu_file, L"&File");
+	AppendMenu(menu_file, MF_STRING, W_OPEN, L"&Open");
+	AppendMenu(menu_file, MF_STRING, W_SAVE, L"&Save");
+	AppendMenu(menu_file, MF_STRING, W_PRINT, L"&Print");
+	AppendMenu(menu_file, MF_STRING, W_EXIT, L"&Exit");
 	AppendMenu(main_menu, MF_STRING | MF_POPUP, (UINT)menu_draw, L"&Draw");
-	AppendMenu(menu_file, MF_STRING, 1001, L"...");
 	AppendMenu(menu_draw, MF_STRING, W_PEN, L"&Pen");
-	AppendMenu(menu_draw, MF_STRING, W_BRUSH, L"&Brush");
 	AppendMenu(menu_draw, MF_STRING | MF_POPUP, (UINT)menu_figure, L"&Figure");
 	AppendMenu(menu_figure, MF_STRING, W_LINE, L"&Line");
 	AppendMenu(menu_figure, MF_STRING, W_CURVE, L"&Curve");
 	AppendMenu(menu_figure, MF_STRING, W_ELLIPSE, L"&Ellipse");
 	AppendMenu(menu_figure, MF_STRING, W_RECTANGLE, L"&Rectangle");  
+	AppendMenu(menu_figure, MF_STRING | MF_POPUP, (UINT)menu_polygon, L"&Polygon");  
+	AppendMenu(menu_polygon, MF_STRING, W_P3, L"&3");  
+	AppendMenu(menu_polygon, MF_STRING, W_P4, L"&4"); 
+	AppendMenu(menu_polygon, MF_STRING, W_P5, L"&5"); 
+	AppendMenu(menu_polygon, MF_STRING, W_P6, L"&6"); 
+	AppendMenu(main_menu, MF_STRING | MF_POPUP, (UINT)menu_color, L"&Color");
+	AppendMenu(menu_color, MF_STRING, W_COLORPEN, L"&Pen...");
+	AppendMenu(menu_color, MF_STRING, W_COLORBRUSH, L"&Brush...");
+	AppendMenu(main_menu, MF_STRING | MF_POPUP, (UINT)menu_canvas, L"&Canvas");
+	AppendMenu(menu_canvas, MF_STRING, W_CLEAR, L"&Clear");
  
 	SetMenu(hWnd, main_menu);
 
@@ -141,10 +165,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		SelectObject ( memDC2, memBM2);
 		FillRect(memDC,&lprect, Brush);
 		FillRect(memDC2,&lprect, Brush);
-		//ps.hdc = hdc;
-		//ps.rcPaint = lprect;
+		ps.hdc = hdc;
+		ps.rcPaint = lprect;
         break;
-	case WM_PAINT :															//IT DOESN'T WORK!!!!!
+	case WM_PAINT :
 		hdc = BeginPaint(hWnd, &ps);
 		BitBlt(hdc, 0, 0, lprect.right, lprect.bottom, memDC2, 0, 0, SRCCOPY);
 		EndPaint(hWnd,&ps);
@@ -157,19 +181,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 
 		HMENU hMenu;
+		
 		switch (wParam) 
 		{
+		case W_OPEN:
+			currentFigure = NONE;
+			break;
+		case W_SAVE:
+			currentFigure = NONE;
+			break;
+		case W_PRINT:
+			currentFigure = NONE;
+			break;
+		case W_EXIT:
+			currentFigure = NONE;
+			break;
 		case W_PEN:
-			currentTool = PEN;
+			currentFigure = PEN;
 			break;
-		case W_BRUSH:
-			currentTool = BRUSH;
-			break;
-		default: break;
-		}
-
-		switch (wParam) 
-		{
 		case W_LINE:
 			currentFigure = LINE;
 			break;
@@ -182,6 +211,29 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		case W_CURVE:
 			currentFigure = CURVE;
 			break;
+		case W_P3:
+			currentFigure = P3;
+			break;
+		case W_P4:
+			currentFigure = P4;
+			break;
+		case W_P5:
+			currentFigure = P5;
+			break;
+		case W_P6:
+			currentFigure = P6;
+			break;
+		case W_COLORPEN:
+			
+			break;
+		case W_COLORBRUSH:
+			
+			break;
+		case W_CLEAR:
+			FillRect(memDC,&lprect, Brush);
+			FillRect(memDC2,&lprect, Brush);
+			InvalidateRect(hWnd,&lprect,false);
+			break;
 		default: break;
 		}
 		break;
@@ -189,20 +241,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_MOUSEMOVE:
 		if (fTracking)
 		{
-			MouseMoveAction(hWnd, lParam, &ptsBegin, currentFigure, currentTool);
+			MouseMoveAction(hWnd, lParam, &ptsBegin, currentFigure);
 		}
 		break;
 	case WM_LBUTTONUP:
 		BitBlt(memDC, 0, 0, lprect.right, lprect.bottom, memDC2, 0, 0, SRCCOPY);
         fTracking = FALSE;
-        ClipCursor(NULL);						// free cursor
+        ClipCursor(NULL);					// free cursor
         ReleaseCapture();
 		InvalidateRect(hWnd,&lprect,false);
         return 0;
     case WM_DESTROY:
 		ReleaseDC(hWnd, hdc);				// free DC
 		ReleaseDC(hWnd, memDC);				// free memDC
-		ReleaseDC(hWnd, memDC2);				// free memDC2
+		ReleaseDC(hWnd, memDC2);			// free memDC2
         PostQuitMessage(0);
         break;
     default:
@@ -212,16 +264,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     return 0;
 }
-int MouseMoveAction(HWND hWnd, LPARAM lParam, POINTS *ptsBegin, figures currentFigure, tools currentTool)
+int MouseMoveAction(HWND hWnd, LPARAM lParam, POINTS *ptsBegin, figures currentFigure)
 {
     POINTS ptsEnd;
 
-	ptsEnd = MAKEPOINTS(lParam);		// get the end coords in POINTS format
+	ptsEnd = MAKEPOINTS(lParam);			// get the end coords in POINTS format
 	MoveToEx(memDC2, ptsBegin->x, ptsBegin->y, (LPPOINT) NULL);
 	Brush = ( HBRUSH ) GetStockObject( HOLLOW_BRUSH );
 	hOldBush = SelectObject( memDC2, Brush );
-	if (currentTool == PEN)
-	{
+	
 		switch (currentFigure)
 		{
 		case LINE:
@@ -248,10 +299,6 @@ int MouseMoveAction(HWND hWnd, LPARAM lParam, POINTS *ptsBegin, figures currentF
 			ptsBegin->y = ptsEnd.y;
 			break;
 		}
-	}
-	if (currentTool == BRUSH)
-	{
-	}
 
 	return 0;
 }
